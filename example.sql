e56cdd7f3061abfa868dd9700f05ffe805163282
kirmessi <khitsenko@bk.ru>
2019-02-06 12:59:16 +0200

    dump sql

--- example.sql
+++ example.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Фев 01 2019 г., 18:12
+-- Время создания: Фев 06 2019 г., 12:58
 -- Версия сервера: 5.7.25-0ubuntu0.18.04.2
 -- Версия PHP: 7.2.10-0ubuntu0.18.04.1
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `category_id` int(11) NOT NULL,
-  `name` text NOT NULL,
+  `title` varchar(250) NOT NULL,
   `slug` text NOT NULL,
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -37,7 +37,7 @@ CREATE TABLE `categories` (
 -- Дамп данных таблицы `categories`
 --
 
-INSERT INTO `categories` (`category_id`, `name`, `slug`, `description`) VALUES
+INSERT INTO `categories` (`category_id`, `title`, `slug`, `description`) VALUES
 (1, 'Category1', 'category-one', 'Полное описание категории1'),
 (5, 'Category2', 'category-two', 'Полное описание категории2');
 
@@ -49,11 +49,11 @@ INSERT INTO `categories` (`category_id`, `name`, `slug`, `description`) VALUES
 
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
   `slug` text NOT NULL,
   `category_id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `description` text NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL,
   `text` text NOT NULL,
   `visibility` tinyint(1) DEFAULT NULL,
   `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
@@ -63,8 +63,10 @@ CREATE TABLE `posts` (
 -- Дамп данных таблицы `posts`
 --
 
-INSERT INTO `posts` (`id`, `user_id`, `slug`, `category_id`, `name`, `description`, `text`, `visibility`, `date`) VALUES
-(1, 3, 'test', 1, 'Первый пост', 'описание поста', 'asdasdasdasdasdasdasd', 1, '2019-02-01 16:07:51');
+INSERT INTO `posts` (`id`, `author_id`, `slug`, `category_id`, `title`, `description`, `text`, `visibility`, `date`) VALUES
+(5, 4, 'post2', 1, 'turb2', 'description2', 'descriptiondescriptiondescriptiondescriptiondescription22', 1, '2019-02-03 16:35:45'),
+(24, 1, 'test1312', 5, 'Кирилл', 'description', 'adasdsadasda', 1, '2019-02-03 16:32:35'),
+(25, 4, 'post3', 5, 'turb34', 'description', 'descriptiondescriptiondescriptiondescriptiondescription', 1, '2019-02-03 16:35:45');
 
 -- --------------------------------------------------------
 
@@ -74,7 +76,7 @@ INSERT INTO `posts` (`id`, `user_id`, `slug`, `category_id`, `name`, `descriptio
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(150) NOT NULL,
+  `username` varchar(250) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(250) NOT NULL,
   `user_registered` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -85,8 +87,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `user_registered`) VALUES
-(1, 'admin', 'test@bk.ru', '12345', '2019-02-01 17:36:52'),
-(3, 'test', 'test23@bk.ru', '202cb962ac59075b964b07152d234b70', '2019-02-01 17:37:25');
+(1, 'admin', 'kirmessi2394@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2019-02-03 18:28:03'),
+(4, 'test', 'testnew@bk.ru', '202cb962ac59075b964b07152d234b70', '2019-02-03 18:33:10');
 
 --
 -- Индексы сохранённых таблиц
@@ -103,19 +105,15 @@ ALTER TABLE `categories`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username_3` (`user_id`),
   ADD KEY `category_id` (`category_id`),
   ADD KEY `category_id_2` (`category_id`),
-  ADD KEY `username` (`user_id`),
-  ADD KEY `username_2` (`user_id`),
-  ADD KEY `username_4` (`user_id`);
+  ADD KEY `user_id` (`author_id`);
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -125,17 +123,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
@@ -145,7 +143,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
